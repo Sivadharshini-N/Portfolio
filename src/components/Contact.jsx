66456635de19ef90c66dd4026c993ce93e15c0d8
Sivadharshini-N <nsivadharshini1@gmail.com>
2024-08-29 07:17:27 +0530

    small change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const contact = () => {
             <div className="text-center"><br />
               <h2>Reach Out</h2><br /><br />
               <div className="cotainer">
-                <form action="mailto:nsivadharshini1@gmail.com" encType='text/plain' style={{color:"#bbff33"}}>
+                <form action="mailto:nsivadharshini1@gmail.com" style={{color:"#bbff33"}}>
                 Name:{" "} <input type="text" id='name' className='rounded' placeholder='Enter your name' /><br /><br />
                 email:{" "} <input type="text" id='email' className='rounded' placeholder='Email' /> <br /><br />
                 <label htmlFor="body">Message</label><br /><br />  <textarea name="body" id="body" placeholder='Share your valuable message' className='rounded text-area'></textarea>
